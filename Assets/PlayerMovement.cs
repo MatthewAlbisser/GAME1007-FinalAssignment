aefc13257f5a8a4486bb4d1ea0eb419dfbd53e78
Weezil <200568840@student.georgianc.on.ca>
2024-04-12 16:41:50 -0400

    Upgraded movement
    added boosting

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,13 +7,14 @@ public class PlayerMovement : MonoBehaviour
     private float xDir;
     private float yDir;
 
-    public float speed;
+    private float speed;
+    private float boost;
     // Start is called before the first frame update
     void Start()
     {
         if (speed == 0)
         {
-            speed = 0.02f;
+            speed = 0.01f;
         }
     }
 
@@ -23,11 +24,11 @@ public class PlayerMovement : MonoBehaviour
         //Get values
         if (Input.GetKey(KeyCode.W))
         {
-            yDir = speed;
+            yDir = speed + boost;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            yDir = -speed;
+            yDir = -speed - boost;
         }
         else
         {
@@ -36,16 +37,24 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.D))
         {
-            xDir = speed;
+            xDir = speed + boost;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            xDir = -speed;
+            xDir = -speed - boost;
         }
         else
         {
             xDir = 0;
         }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            boost = speed;
+        }
+        else { boost = 0; }
+
+
 
         //Apply values
         gameObject.transform.position = new Vector3(gameObject.transform.position.x + xDir, gameObject.transform.position.y + yDir, gameObject.transform.position.z);
