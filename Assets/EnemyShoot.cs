b62f35fe3d9f85d2ec1b18f4a227afb82f69f4b1
Joshua Hardwick <200560630@student.georgianc.on.ca>
2024-04-12 19:49:55 -0400

    enemy shoots at player

--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -6,34 +6,35 @@ public class EnemyShoot : MonoBehaviour
 {
     public GameObject player;
     public GameObject Bullet;
-    public Transform shootPoint;
     public bool inVision;
     public float cooldown = 3;
     private Vector3 BulletDirection = Vector3.zero;
-    private bool lineOfSight = false;
+    public bool lineOfSight = false;
 
     void Update()
     {
+        cooldown -= Time.deltaTime;
         player.transform.position = player.GetComponent<Transform>().position;
 
-        if (lineOfSight && inVision)
+        if (lineOfSight)
         {
             BulletDirection = (player.transform.position - transform.position).normalized;
             if (cooldown < 0)
             {
-                GameObject enemyBulletClone = GameObject.Instantiate(Bullet, shootPoint.transform.position + BulletDirection, Quaternion.identity);
+                GameObject enemyBulletClone = GameObject.Instantiate(Bullet, transform.position + BulletDirection, Quaternion.identity);
                 enemyBulletClone.GetComponent<Rigidbody>().velocity = BulletDirection * 20;
                 Destroy(enemyBulletClone, 5f);
+                cooldown = 3;
             }
             
         }
 
-        RaycastHit2D ray = Physics2D.Raycast(Vector2.zero, Vector2.zero);
-        ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
-        Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
-        
-        
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hitInfo))
+        {
+            lineOfSight = hitInfo.collider.CompareTag("Player");
+            Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+        }
 
-        
     }
 }
