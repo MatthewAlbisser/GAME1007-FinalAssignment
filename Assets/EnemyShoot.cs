eeaa3a9e480cddd1e90f9e78d271152368545c4a
Joshua Hardwick <200560630@student.georgianc.on.ca>
2024-04-12 19:36:04 -0400

    added enemy shooting

--- Assets/EnemyShoot.cs
+++ Assets/EnemyShoot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyShoot : MonoBehaviour
 {
     public GameObject player;
+    public GameObject Bullet;
+    public Transform shootPoint;
     public bool inVision;
     public float cooldown = 3;
     private Vector3 BulletDirection = Vector3.zero;
@@ -17,10 +19,21 @@ public class EnemyShoot : MonoBehaviour
         if (lineOfSight && inVision)
         {
             BulletDirection = (player.transform.position - transform.position).normalized;
+            if (cooldown < 0)
+            {
+                GameObject enemyBulletClone = GameObject.Instantiate(Bullet, shootPoint.transform.position + BulletDirection, Quaternion.identity);
+                enemyBulletClone.GetComponent<Rigidbody>().velocity = BulletDirection * 20;
+                Destroy(enemyBulletClone, 5f);
+            }
+            
         }
 
         RaycastHit2D ray = Physics2D.Raycast(Vector2.zero, Vector2.zero);
         ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
         Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+        
+        
+
+        
     }
 }
