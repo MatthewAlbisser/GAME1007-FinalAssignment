3a9f758a18edfc7aa73a2bc0dfb43ace02b8b596
MatthewAlbisser <144923804+MatthewAlbisser@users.noreply.github.com>
2024-04-12 20:10:23 -0400

    Bullet Script

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 velocity;
+    private float speed = 5;
+    void Start()
+    {
+        velocity = transform.forward * speed;
+    }
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Vector3 normal = collision.contacts[0].normal;
+            velocity = Vector3.Reflect(velocity, normal);
+        }
+    }
+}
